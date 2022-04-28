9132965095c63f162607d58838151f6c166a073d
toma <toma95s@tomas-MBP.lan>
2022-04-28 15:54:59 -0700

    added notes route, fixed css file route and location, modified packagejson, added elements to noteshtml, downloaded npm, recreated assets routes

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const fs = require("fs")
+//const api = require('./routes/index.js');
+
+const PORT = process.env.port || 3002;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//app.use('/api', api);
+
+app.use(express.static('public'));
+app.get('/api/notes',(req,res)=> {
+    fs.readFile("lib/db/db.json","utf-8",(error,data)=>{
+        if (error){
+            console.log (error)
+        }
+        res.json(JSON.parse(data))
+    })
+})
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname,'/public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
