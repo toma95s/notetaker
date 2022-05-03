6c65cb296879ecb86f96acd12ddd5ca6dfd4927d
toma <toma95s@tomas-MBP.lan>
2022-05-03 15:23:12 -0700

    added post functionality

--- server.js
+++ server.js
@@ -11,23 +11,40 @@ app.use(express.urlencoded({ extended: true }));
 //app.use('/api', api);
 
 app.use(express.static('public'));
-app.get('/api/notes',(req,res)=> {
-    fs.readFile("lib/db/db.json","utf-8",(error,data)=>{
-        if (error){
-            console.log (error)
-        }
-        res.json(JSON.parse(data))
-    })
+app.get('/api/notes', (req, res) => {
+  fs.readFile("lib/db/db.json", "utf-8", (error, data) => {
+    if (error) {
+      console.log(error)
+    }
+    res.json(JSON.parse(data))
+  })
 })
+app.post('/api/notes', (req, res) => {
+  fs.readFile("lib/db/db.json", "utf-8", (error, data) => {
+    if (error) {
+      console.log(error)
+    }
+    //create new note
+    const newNote = req.body
+    const notes = JSON.parse(data)
+    notes.push(newNote)
+    fs.writeFile("lib/db/db.json", JSON.stringify(notes), (err)=>{
+      res.json(newNote)
+    })
+  })
+}
+);
+
 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname,'/public/index.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
+
